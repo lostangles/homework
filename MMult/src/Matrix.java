e014da75bab9bdae57114764803b4b7d09044c8c
lostangles <branjb@gmail.com>
2016-02-13 16:11:16 -0800

    Added Strassen algorithm

---
+++ MMult/src/Matrix.java
@@ -0,0 +1,161 @@
+/**
+ * 
+ */
+
+/**
+ * @author Brandon
+ *
+ */
+
+public class Matrix {
+
+	//Fields
+	int r;
+	int c;
+	int data[][];// = new int[r][c];
+	
+	
+	//Constructor
+	public Matrix () {
+		r = 1;
+		c = 1;
+		data = new int[r][c];
+		data[0][0] = 1;
+	}
+	
+	public Matrix (int values[], int n) {
+		int tracker = 0;
+		this.r = n;
+		this.c = n;
+		this.data = new int[r][c];
+		for (int i=0 ; i<n ; i++)
+			for (int j=0 ; j < n ; j++)
+			{
+				this.data[i][j] = values[tracker];
+				tracker += 1;
+			}
+	}
+	
+	public Matrix (int n) {
+		this.r = n;
+		this.c = n;
+		this.data = new int[r][c];
+	}
+	
+	public void printMatrix () {
+		for (int i = 0 ; i < r; i++) {
+			for (int j = 0 ; j < c ; j++) {
+				System.out.print(data[i][j] + " ");
+				if (j == c-1) System.out.print("\n");
+			}
+		}
+		System.out.println();
+	}
+	
+	public Matrix addMatrix(Matrix a, Matrix b, int n) {
+		Matrix c = new Matrix(n);
+		for (int i = 0 ; i< n ; i++) 
+			for (int j=0 ; j< n ; j++) {
+				c.data[i][j] = a.data[i][j] + b.data[i][j];
+			}
+		
+		
+		return c;
+	}
+	
+	public Matrix subMatrix(Matrix a, Matrix b, int n) {
+		Matrix c = new Matrix(n);
+		for (int i = 0 ; i< n ; i++) 
+			for (int j=0 ; j< n ; j++) {
+				c.data[i][j] = a.data[i][j] - b.data[i][j];
+			}
+		
+		
+		return c;
+	}
+	
+	public Matrix multonebyone(Matrix a, Matrix b) {
+		Matrix c = new Matrix(1);
+		c.data[0][0] = a.data[0][0] * b.data[0][0];
+		return c;
+	}
+	
+	public Matrix copy(Matrix m, int start_row, int start_column) {
+		Matrix c = new Matrix(m.r/2);
+		for (int i = 0; i < c.r ; i++)
+			for (int j = 0 ; j < c.c ; j++) {
+				c.data[i][j] = m.data[i + start_row][j + start_column];
+			}
+		return c;
+	}
+	
+	public Matrix merge(Matrix topleft, Matrix topright, Matrix bottomleft, Matrix bottomright) {
+		Matrix c = new Matrix();
+		c.r = topleft.r + bottomright.r;
+		c.c = topleft.c + topright.c;
+		c.data = new int[c.r][c.c];
+		
+		for (int i=0; i<c.c ; i++)
+			for (int j=0; j<c.r ; j++) {
+				if ( (i < topleft.c) && (j < topleft.r )) c.data[i][j] = topleft.data[i][j];
+				if ( (i < topleft.c) && (j >= topleft.r )) c.data[i][j] = topright.data[i][j-topleft.r];
+				if ( (i >= topleft.c) && (j < topleft.r )) c.data[i][j] = bottomleft.data[i-topleft.r][j];
+				if ( (i >= topleft.c) && (j >= topleft.r )) c.data[i][j] = bottomright.data[i-topleft.r][j-topleft.r];
+
+			}
+		
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Test matrix, 8 x 8      :  [ 0 1 2 3 4 5 6 7
+		//								1 2 3 4 5 6 7 8
+		//								2 3 4 5 6 7 8 9
+		//								3 4 5 6 7 8 9 10
+		//								4 5 6 7 8 9 10 11
+		//								5 6 7 8 9 10 11 12
+		//								6 7 8 9 10 11 12 13
+		//								7 8 9 10 11 12 13 14 
+		int array[] = {0,1,2,3,4,5,6,7,1,2,3,4,5,6,7,8,2,3,4,5,6,7,8,9,3,4,5,6,7,8,9,10,4,5,6,7,8,9,10,11,5,6,7,8,9,10,11,12,6,7,8,9,10,11,12,13,7,8,9,10,11,12,13,14};
+		int size = 8;
+		Matrix a = new Matrix (array, size);
+		a.printMatrix();
+		
+		Matrix b = new Matrix (array, size);
+		Matrix c = new Matrix (size);
+		size = 8;
+		c = c.addMatrix(a, b, size);
+		System.out.println();
+		c.printMatrix();
+		System.out.println();
+		c = c.subMatrix(c, b, size);
+		c.printMatrix();
+		System.out.println();
+
+		
+		//Test copy
+		Matrix copy = c.copy(c, 0, 4);
+		copy.printMatrix();
+		a = a.addMatrix(a, b, size);
+		
+		int n = 4;
+		//Test subdivide
+		Matrix A11, A12, A21, A22;
+		A11 = a.copy(a, 0, 0);
+		A12 = a.copy(a, 0, n);
+		A21 = a.copy(a, n, 0);
+		A22 = a.copy(a, n, n);
+		
+		a.printMatrix();
+		A11.printMatrix();
+		A12.printMatrix();
+		A21.printMatrix();
+		A22.printMatrix();
+		
+		a = a.merge(A11, A12, A21, A22);
+		a.printMatrix();
+		
+	}
+
+}
---
+++ MMult/src/Strassen.java
@@ -0,0 +1,64 @@
+
+public class Strassen {
+
+	public static Matrix Strassen(Matrix a, Matrix b, int size) {
+		int n = size/2;
+		Matrix c = new Matrix();
+		
+		if (size == 1) {
+			c = c.multonebyone(a, b);
+			return c;
+		}
+		
+		Matrix A11, A12, A21, A22;
+		Matrix B11, B12, B21, B22;
+		Matrix C11, C12, C21, C22;
+		Matrix P1, P2, P3, P4, P5, P6, P7;
+		
+		//Subdivide matrixes
+		A11 = a.copy(a, 0, 0);
+		A12 = a.copy(a, 0, n);
+		A21 = a.copy(a, n, 0);
+		A22 = a.copy(a, n, n);
+		
+		B11 = b.copy(b, 0, 0);
+		B12 = b.copy(b, 0, n);
+		B21 = b.copy(b, n, 0);
+		B22 = b.copy(b, n, n);
+		
+		P1 = Strassen(A11, B12.subMatrix(B12, B22, n), n);
+		P2 = Strassen(A11.addMatrix(A11, A12, n), B22, n);
+		P3 = Strassen(A21.addMatrix(A21, A22, n), B11, n);
+		P4 = Strassen(A22, B21.subMatrix(B21, B11, n), n);
+		P5 = Strassen(A11.addMatrix(A11, A22, n), B11.addMatrix(B11, B22, n), n);
+		P6 = Strassen(A12.subMatrix(A12, A22, n), B21.addMatrix(B21, B22, n), n);
+		P7 = Strassen(A11.subMatrix(A11, A21, n), B11.addMatrix(B11, B12, n), n);
+		C11 = P5.addMatrix(P5, P4, n);
+		C11 = C11.subMatrix(C11, P2, n);
+		C11 = C11.addMatrix(C11, P6, n);
+		C12 = P1.addMatrix(P1, P2, n);
+		C21 = P3.addMatrix(P3, P4, n);
+		C22 = P1.addMatrix(P1, P5, n);
+		C22 = C22.subMatrix(C22, P3, n);
+		C22 = C22.subMatrix(C22, P7, n);
+		
+		c = c.merge(C11, C12, C21, C22);
+		
+		return c;
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = { 1 , 2 , 3, 4,2,3,4,5,3,4,5,6,4,5,6,7 };
+		int size = 4;
+		Matrix a = new Matrix(array, size);
+		Matrix b = new Matrix(array, size);
+		a.printMatrix();
+		b.printMatrix();
+		
+		Matrix c = Strassen(a,b,4);
+		c.printMatrix();
+	}
+
+}
